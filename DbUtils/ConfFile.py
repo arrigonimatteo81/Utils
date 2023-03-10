52245852435d400b596db092c1fa0bbbcb4514d7
matteo <matteo.arrigoni81@gmail.com>
2023-03-10 17:00:53 +0100

    aggiunta classe per parametri source da file config.ini

---
+++ DbUtils/ConfFile.py
@@ -0,0 +1,28 @@
+from DbUtils.interfaces.IDbConf import IDbConf
+
+
+class ConfFile(IDbConf):
+
+    def __init__(self, config):
+        self.config = config
+
+    def getCountQuery(self, table) -> str:
+        return self.config[table]["Sql_String_Count"]
+
+    def getIngestionQuery(self, table) -> str:
+        return self.config[table]["Sql_String_Ingestion"]
+
+    def getIngestionTable(self, table) -> str:
+        return self.config[table]["Output_Table"]
+
+    def getSparkParameters(self, table) -> dict:
+        return self.config[table]["Spark_Params"] ##ATTENZIONE: JSON!
+
+    def getAdditionalWhere(self, table) -> str:
+        return self.config[table]["Additional_Where"]
+
+    def getNumPartitions(self, table) -> int:
+        return self.config[table]["Num_Partitions"]
+
+    def getSourceParameters(self, table) -> dict:
+        return self.config[table]["Db_Source_Params"] ##ATTENZIONE: JSON!
